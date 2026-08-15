2146009b2b917fbfde6fa1344c5abb5b48639341
narekmarkosyan <narek.markosyan@yahoo.com>
2026-08-15 17:31:53 +0400

    dynamic name for admin routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Narekmarkosyan\LaravelAdminTelegramTwoFactor\Http\Controllers\AuthController;
 
-Route::get('auth/2fa', [AuthController::class, 'getTwoFactor'])->name('admin.auth.2fa.telegram')->withoutMiddleware('admin.auth.2fa.telegram');
-Route::post('auth/2fa', [AuthController::class, 'postTwoFactor'])->name('admin.auth.2fa.telegram')->withoutMiddleware('admin.auth.2fa.telegram');
-Route::get('auth/2fa/resend', [AuthController::class, 'getTwoFactorResend'])->name('admin.auth.2fa.telegram.resend')->withoutMiddleware('admin.auth.2fa.telegram');
+Route::get('auth/2fa', [AuthController::class, 'getTwoFactor'])->name('auth.2fa.telegram')->withoutMiddleware('admin.auth.2fa.telegram');
+Route::post('auth/2fa', [AuthController::class, 'postTwoFactor'])->name('auth.2fa.telegram')->withoutMiddleware('admin.auth.2fa.telegram');
+Route::get('auth/2fa/resend', [AuthController::class, 'getTwoFactorResend'])->name('auth.2fa.telegram.resend')->withoutMiddleware('admin.auth.2fa.telegram');
